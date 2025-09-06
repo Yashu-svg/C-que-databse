24d17da6870601625dc6c4dcbcd2a63664a496d3
Yashu-svg <dashingyash07@gmail.com>
2025-09-06 10:59:39 +0530

    Q2 50:42
    Q2Write a program to calculate area of circle(side is given)

---
+++ Q2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+//Q2Write a program to calculate area of circle(side is given)
+//50:42
+{
+    float radius;
+    printf("Enter Radius: ");
+    scanf("%f",&radius);
+
+    printf("area is : %f",3.14*radius*radius);
+    return 0;
+}
