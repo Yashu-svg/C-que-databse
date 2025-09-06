1283c58412f572fb23bb4994fcbf7431c3ff697b
Yashu-svg <dashingyash07@gmail.com>
2025-09-06 10:49:47 +0530

    Q1 48:02
    Write a program to calculate area of square

---
+++ Q1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//48:02
+//area of a square
+int main(){
+    float side;
+    printf("Enter side :");
+    scanf("%f",&side);
+
+    printf("area is : %.1f",side*side);
+    return 0;
+
+}
